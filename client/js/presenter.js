92533b0923d6243e5d92ee75a0ea4dd1cd62b22b
Ahmed Modan <ahmed.modan@gmail.com>
2016-04-16 19:01:49 -0700

    [feature] basic functionality for finding new related gifs

--- client/js/presenter.js
+++ client/js/presenter.js
@@ -1,20 +1,43 @@
 var Presenter = {
   load: function (event) {
+    var loadingDoc;
     var self = this
     var gifObj = {};
     gifObj.url = event.target.getAttribute('url');
     gifObj.preview = event.target.getAttribute('preview');
     gifObj.tags = event.target.getAttribute('tags');
     gifObj.id = event.target.getAttribute('id');
+
+
     resourceLoader.loadResource(
-      `${resourceLoader.BASEURL}templates/relatedGifDisplay.xml.js`,
-      gifObj,
+      `${resourceLoader.BASEURL}templates/loading.xml.js`,
+      'Loading...',
       function (resource) {
-        var doc = self.makeDocument(resource);
-        doc.addEventListener("select", self.load.bind(self));
-        self.pushDocument(doc);
+        loadingDoc = self.makeDocument(resource);
+        loadingDoc.addEventListener("select", self.load.bind(self));
+        self.pushDocument(loadingDoc);
       }
     );
+
+    resourceLoader.getGifs(gifObj.tags.split(',').join('+'), function (response) {
+      gifObj.related = response.results.map(function (gif) {
+        var gifObject = {
+          media: gif.media[0].gif,
+          tags: gif.tags,
+          id: gif.id
+        }
+        return gifObject
+      });
+      resourceLoader.loadResource(
+        `${resourceLoader.BASEURL}templates/relatedGifDisplay.xml.js`,
+        gifObj,
+        function (resource) {
+          var doc = self.makeDocument(resource);
+          doc.addEventListener("select", self.load.bind(self));
+          self.replaceDocument(doc, loadingDoc);
+        }
+      );
+    })
   },
 
 
@@ -29,6 +52,10 @@ var Presenter = {
 
   modalDialogPresenter: function (xml) {
     navigationDocument.presentModal(xml);
+  },
+
+  replaceDocument: function (newDoc, oldDoc) {
+    navigationDocument.replaceDocument(newDoc, oldDoc);
   },
 
   pushDocument: function (xml) {
